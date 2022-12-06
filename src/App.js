a7495a4f4add2a55daa255099c4256ddabe7e488
Ashish Chaudhary <ashish080303@gmail.com>
2022-12-06 13:08:19 +0530

    app done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 let color = "blue";
-let headText = "Ashish";
+let headText = "Ashish's Practice Project";
 const array = ["milk", "eggs", "peanut", "butter"];
 const url = "https://unpkg.com/emoji.json@13.1.0/emoji.json";
 
 export default function App() {
-  const [emoji, setEmoji] = useState("");
-  const [rand, setRand] = useState("");
+  const [emojiName, setEmojiName] = useState("");
+  const [emoji,setEmoji] = useState("type");
+  const [rand, setRand] = useState([]);
   const [data, setData] = useState("");
 
   useEffect(()=>{
@@ -16,12 +17,11 @@ export default function App() {
     .then((json) => {
       setData(json);
     });
-   },[]) 
+   },[""]) 
 
   useEffect(() => {
     renderEmoji()
   },[data])
-
 
 
   function doThis(event) {
@@ -29,9 +29,8 @@ export default function App() {
 
         data.map((item) => {
           if (item.char === input) {
-            setEmoji(item.name);
+            setEmojiName(item.name);
           }
-          return item.array;
         });
       };
 
@@ -39,24 +38,32 @@ export default function App() {
     console.log("clicked", item);
   }
 
-  function getRandomInt(number){
-    return Math.floor(Math.random()*number);
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function emojisClicked(i){
+    setEmoji(i);
+    data.map((item) => {
+      if (item.char === i) {
+        setEmojiName(item.name);
+      }
+    });
   }
 
   function renderEmoji(){
+    let d = []
       for (let i = 0; i < 5; i++) {
         let some = getRandomInt(4590);
-        setRand(data[some]?.char);
-  
+        d = [...d, data[some]?.char ]
       }
+      setRand(d);
   }
-
   return (
     <div className="App">
       <h1 style={{ color: color }} className="main-head">
         {headText}
       </h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>This is a list for practice </h2>
 
       <ul>
         {array.map((item, index) => {
@@ -76,11 +83,18 @@ export default function App() {
       </ul>
 
       <h1> Emoji Shower </h1>
-      <input onChange={doThis} placeholder="type here"></input>
-      <h3 className="emoji-name"> = {emoji}</h3>
+      <h5>Enter an emoji to see magic!</h5>
+      <input onChange={doThis} placeholder={emoji} id="emojiInput"></input>
+      <h3 className="emoji-name"> = {emojiName}</h3>
       <section>
         <h2 className="stock">emoji's on the stock</h2>
-        {rand}
+
+        <div className="emojis">
+        {rand.map(i => [
+          // if I am adding the curly brackets instead of square here, code doesn't work. Why's that?
+          <span onClick={()=> emojisClicked(i)} id="eachEmoji" key={i}>{i}</span>
+        ])}
+        </div>
       </section>
     </div>
   );
